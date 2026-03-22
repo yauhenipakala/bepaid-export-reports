7d051e02f5405cb8e56b20af35d864a52ea718b1
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2026-03-22 22:46:45 +0100

    Init nodejs project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+console.log('Environment variables loaded:', process.env.TEST);
+console.log('Starting the report export process...');
---
+++ src/index.test.js
@@ -0,0 +1,5 @@
+import { describe, expect, test } from '@jest/globals';
+
+test('1 equals 1', () => {
+  expect(1).toBe(1);
+});
